2397fb81af048be5e61e19b8c22f75b769f6df0f
hys <helpyourselfes@gmail.com>
2025-10-04 11:16:09 +0300

    CallView with useData

---
+++ src/data/functions/useData.ts
@@ -0,0 +1,27 @@
+import { ref } from "vue"
+
+const useData = <T>(loadF: () => Promise<T>) => {
+    const data = ref<T | null>(null)
+    const error = ref<any>(null)
+    const loading = ref(false)
+
+    async function load() {
+        loading.value = true;
+        error.value = false;
+        try {
+            const res = await loadF();
+            data.value = res;
+        } catch (e) {
+            error.value = e
+        } finally {
+            loading.value = false;
+        }
+    }
+
+    load();
+
+    return {data, error, loading, reload: () => load()}
+}
+
+
+export default useData
