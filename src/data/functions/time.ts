3fda2edca07590ce1ef57b52357a2de622b26b74
hys <helpyourselfes@gmail.com>
2025-11-01 18:53:03 +0300

    add time funtions

--- src/data/functions/time.ts
+++ src/data/functions/time.ts
@@ -1,19 +1,40 @@
-import type { period, time } from '../types'
+import type { time } from '../types'
 
 const addTime = (t: time, minutes: number): time => {
   const total = t.h * 60 + t.m + minutes
-  return { h: Math.floor(total / 60), m: total % 60 }
+  return toTime(total)
 }
 
 const toMinutes = (t: time) => t.h * 60 + t.m
+
+const toTime = (t: number): time => ({ h: Math.floor(t / 60), m: t % 60 })
 
 function parseHHMM(s: string): time {
   const m = s.match(/^(\d{1,2}):(\d{2})$/)
   if (!m) throw new Error('Invalid time')
   const h = parseInt(m[1], 10),
     min = parseInt(m[2], 10)
-  if (min < 0 || min > 59) throw new Error('Invalid minutes')
+  if (min < 0 || min > 59) throw new Error('invalid minutes')
   return { h, m: min }
+}
+
+const isBetween = (time: time, start: time, end: time) => {
+  const timeN = toMinutes(time);
+  const startN = toMinutes(start);
+  const endN = toMinutes(end);
+
+  if (timeN > startN && timeN < endN) {
+    return {
+      isBetween: true,
+      fromStart: toTime(timeN - startN),
+      toEnd: toTime(endN - timeN)
+    }
+  }
+  else {
+    return {
+      isBetween: false
+    }
+  }
 }
 
 export { addTime, toMinutes, parseHHMM }
