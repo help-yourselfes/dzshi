92429f86e624686fff5cee0aa0039fe6b75faadc
hys <helpyourselfes@gmail.com>
2025-11-05 22:41:28 +0300

    decompose router

---
+++ src/router/routes.ts
@@ -0,0 +1,27 @@
+import AboutView from "@/views/AboutView.vue";
+import CallsView from "@/views/CallsView.vue";
+import TasksView from "@/views/TasksView.vue";
+import TimetableView from "@/views/TimetableView.vue";
+import type { RouteRecordRaw } from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: (to: any) => {
+      const date = new Date();
+      return {
+        path: '/tasks',
+        query: {
+          year: date.getFullYear(),
+          month: date.getMonth() + 1,
+          day: date.getDate()
+        }
+      }
+    },
+  },
+  { path: '/tasks', component: TasksView },
+  { path: '/calls/:dayId?', component: CallsView },
+  { path: '/timetable', component: TimetableView },
+  { path: '/about', component: AboutView },
+]
+export default routes
