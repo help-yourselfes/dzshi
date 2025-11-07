2c7f209dfaf3ab5ec7cad35120090f89e71c7765
hys <helpyourselfes@gmail.com>
2025-11-07 23:04:07 +0300

    Call list markup

---
+++ src/components/Desktop/CallView/Call.vue
@@ -0,0 +1,98 @@
+<template>
+    <div v-if="call.type === 'lesson' || isCurrent" :class="{
+        'call': !isCurrent,
+        'current-call': isCurrent,
+        type
+    }">
+        <span :class="isCurrent ? 'active-name' : 'name'">
+            {{ name }}
+        </span>
+        <div class="time">
+            <TimeSpan :time="call.start" class="start" :class="isCurrent && 'fade-text'" />
+            <span v-if="isCurrent" class="remain-time">
+                <RemainMinutes :end="call.end" />
+            </span>
+            <TimeSpan :time="call.end" class="end" :class="isCurrent && 'fade-text'" />
+        </div>
+    </div>
+
+</template>
+<script setup lang="ts">
+import RemainMinutes from '@/components/primitives/RemainMinutes.vue';
+import TimeSpan from '@/components/primitives/TimeSpan.vue';
+import type { callInfo } from '@/data/types';
+import { computed } from 'vue';
+
+const props = defineProps<{
+    isCurrent?: boolean,
+    call: callInfo
+}>();
+
+const name = computed(() => {
+    const call = props.call;
+    const { type } = call;
+    switch (type) {
+        case 'lesson':
+            return call.name
+        case 'break':
+            return 'Перемена'
+        case 'big-break':
+            return "Большая перемена"
+        default:
+            return "Неизвестно"
+    }
+})
+
+const type = computed(() => props.call.type)
+
+</script>
+<style scoped>
+.call,
+.current-call {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.75rem;
+}
+
+.current-call {
+    padding: 2rem;
+}
+
+.fade-text {
+    opacity: 0.5;
+}
+
+.lesson {
+    border-radius: 1rem;
+    background: var(--middle);
+}
+
+.big-break {
+    justify-content: center;
+}
+
+.active-name {
+    font-weight: 600;
+}
+
+.start,
+.end {
+    width: max-content;
+}
+
+.remain-time {
+    max-width: 5rem;
+    width: max-content;
+    text-align: end;
+    line-height: 100%;
+    font-weight: 600;
+}
+
+.time {
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+}
+</style>
--- src/views/Desktop/CallsView.vue
+++ src/views/Desktop/CallsView.vue
@@ -1,12 +1,19 @@
 <template>
     <DaySelect :dayId />
+    <Transition name="loading">
+        <WaitASecond v-if="calls.loading.value" />
+    </Transition>
     <Transition name="calls">
-
+        <main v-if="calls.data">
+            <Call v-for="(call, id) in calls.data.value" :call :is-current="id === currentCallId.data.value" />
+        </main>
     </Transition>
 </template>
 
 <script setup lang="ts">
 import DaySelect from '@/components/Desktop/CallView/DaySelect.vue';
+import WaitASecond from '@/components/primitives/Spinner/WaitASecond.vue';
+import Call from '@/components/Desktop/CallView/Call.vue';
 import type { UseDataResult } from '@/data/functions/useData';
 import type { callInfo } from '@/data/types';
 
